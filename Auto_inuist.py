36cd5b7827d745dcd6076cc78b81a968166accd5
smalljiejie <nuistdingyongjie@gmail.com>
2017-04-20 20:54:07 +0800

    init files

---
+++ Auto_inuist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from urllib import request, parse
+import json
+
+url = 'http://10.255.255.13/index.php/index/login'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+referer = 'http://10.255.255.13'
+origin = 'http://10.255.255.13'
+
+username = '账号'#手机号或学号
+domain = '运营商'#选填ChinaNet\Unicom\CMCC\NUIST
+password = '密码'#加密后的密码，非明文密码，可用chrome抓包
+enablemacauth = '0'
+login_data = parse.urlencode([
+    ('username', username),
+    ('password', password),
+    ('domain', domain),
+    ('enablemacauth', enablemacauth)
+])
+req = request.Request(url)
+req.add_header('Origin', origin)
+req.add_header('User-Agent', user_agent)
+req.add_header('Referer', referer)
+with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+    print('Status:', f.status, f.reason)
+
---
+++ Net_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PING=`ping -c 3 114.114.114.114 | grep '64 bytes' | wc -l`
+NOT_PING="0"
+if [ "$PING" -eq "$NOT_PING" ]
+then
+  python3 /etc/Auto_inuist.py
+fi
+
