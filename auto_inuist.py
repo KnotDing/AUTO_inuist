3637a9ea92052afaf87d20b788da4b9f90d508d6
smalljiejie <nuistdingyongjie@gmail.com>
2019-01-07 23:46:44 +0800

    try delede net_test

--- auto_inuist.py
+++ auto_inuist.py
@@ -9,49 +9,50 @@ import os
 import shutil
 import platform
 
-url = 'http://10.255.255.13/index.php/index/login'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-referer = 'http://10.255.255.13'
-origin = 'http://10.255.255.13'
-first_use = 1
-username = input("Enter your username(Student number or mobile number):\n")
-domain = input("Enter your domain(ChinaNet\\Unicom\\CMCC\\NUIST):\n")
-password = base64.b64encode(str.encode(input("Enter your password:\n")))
-enablemacauth = '0'
-login_data = parse.urlencode([
-    ('username', username),
-    ('password', password),
-    ('domain', domain),
-    ('enablemacauth', enablemacauth)
-])
-req = request.Request(url)
-req.add_header('Origin', origin)
-req.add_header('User-Agent', user_agent)
-req.add_header('Referer', referer)
-with request.urlopen(req, data=login_data.encode('utf-8')) as f:
-    print('Status:', f.status, f.reason)
-if first_use:
-    shutil.copy2(os.getcwd()+'/auto_inuist.py',os.getcwd()+'/auto_inuist_tmp.py')
-    old = os.getcwd()+'/auto_inuist.py'
-    new = os.getcwd()+'/auto_inuist_tmp.py'
-    o = open(old, mode='r')
-    n = open(new, mode='w')
-    for line in o:
-        n.write(line.replace('username = input("Enter your username(Student number or mobile number):\\n")', 'username = \''+username+'\'')\
-        .replace('domain = input("Enter your domain(ChinaNet\\\\Unicom\\\\CMCC\\\\NUIST):\\n")', 'domain = \''+domain+'\'')\
-        .replace('password = base64.b64encode(str.encode(input("Enter your password:\\n")))', 'password = \''+password.decode()+'\'')\
-        .replace('first_use = 1', 'first_use = 0'))
-    o.close()
-    n.close()
-    os.remove(old)
-    os.rename(new, old)
-    if platform.system() == 'Windows':
-        print('Please use the scheduled task to set up automatic login. You need to add net_test.sh to the scheduled task.')
-    else:
-        crontab = os.getcwd()+'/crontab'
-        if not os.path.exists(crontab):
-            f = open(crontab,'w')
-            f.write('7-23/10 * * * * sh '+os.getcwd()+'/net_test.sh\n')
-            f.close()
-            os.system("crontab ./crontab")
-            os.system("service cron restart")
+if os.popen('ping -c 3 114.114.114.114 | grep \'64 bytes\' | wc -l') == 0:
+    url = 'http://10.255.255.13/index.php/index/login'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    referer = 'http://10.255.255.13'
+    origin = 'http://10.255.255.13'
+    first_use = 1
+    username = input('Enter your username(Student number or mobile number):\n')
+    domain = input('Enter your domain(ChinaNet\\Unicom\\CMCC\\NUIST):\n')
+    password = base64.b64encode(str.encode(input('Enter your password:\n')))
+    enablemacauth = '0'
+    login_data = parse.urlencode([
+        ('username', username),
+        ('password', password),
+        ('domain', domain),
+        ('enablemacauth', enablemacauth)
+    ])
+    req = request.Request(url)
+    req.add_header('Origin', origin)
+    req.add_header('User-Agent', user_agent)
+    req.add_header('Referer', referer)
+    with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+        print('Status:', f.status, f.reason)
+    if first_use:
+        shutil.copy2(os.getcwd()+'/auto_inuist.py',os.getcwd()+'/auto_inuist_tmp.py')
+        old = os.getcwd()+'/auto_inuist.py'
+        new = os.getcwd()+'/auto_inuist_tmp.py'
+        o = open(old, mode='r')
+        n = open(new, mode='w')
+        for line in o:
+            n.write(line.replace('username = input(\'Enter your username(Student number or mobile number):\\n\')', 'username = \''+username+'\'')\
+            .replace('domain = input(\'Enter your domain(ChinaNet\\\\Unicom\\\\CMCC\\\\NUIST):\\n\')', 'domain = \''+domain+'\'')\
+            .replace('password = base64.b64encode(str.encode(input(\'Enter your password:\\n\')))', 'password = \''+password.decode()+'\'')\
+            .replace('first_use = 1', 'first_use = 0'))
+        o.close()
+        n.close()
+        os.remove(old)
+        os.rename(new, old)
+        if platform.system() == 'Windows':
+            print('Please use the scheduled task to set up automatic login. You need to add net_test.sh to the scheduled task.')
+        else:
+            crontab = os.getcwd()+'/crontab'
+            if not os.path.exists(crontab):
+                f = open(crontab,'w')
+                f.write('7-23/10 * * * * sh '+os.getcwd()+'/net_test.sh\n')
+                f.close()
+                os.system('crontab ./crontab')
+                os.system('service cron restart')
