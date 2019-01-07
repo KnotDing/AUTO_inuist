f31bd6b216ba6bd8ab4376ff9d620015a536a41f
smalljiejie <nuistdingyongjie@gmail.com>
2019-01-07 23:48:50 +0800

    debug

--- auto_inuist.py
+++ auto_inuist.py
@@ -9,7 +9,7 @@ import os
 import shutil
 import platform
 
-if os.popen('ping -c 3 114.114.114.114 | grep \'64 bytes\' | wc -l') == 0:
+if os.popen('ping -c 3 114.114.114.114 | grep \'64 bytes\' | wc -l') == '0':
     url = 'http://10.255.255.13/index.php/index/login'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     referer = 'http://10.255.255.13'
