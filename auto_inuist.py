c1e5c6f7d867a937955f2001417441157c431505
smalljiejie <smalljiejie@foxmail.com>
2019-01-21 17:52:16 +0800

    debug

--- auto_inuist.py
+++ auto_inuist.py
@@ -37,44 +37,38 @@ def connect_network():
     req.add_header('Referer', referer)
     with request.urlopen(req, data=login_data.encode('utf-8')) as f:
         print('Status:', f.status, f.reason)
+    return username,domain,password
+def scheduled_task():    
+    if platform.system() == 'Windows':
+        print('Please use the scheduled task to set up automatic login. You need to add auto_nuist.py to the scheduled task.')
+    else:
+        crontab = os.getcwd()+'/crontab'
+        if not os.path.exists(crontab):
+            c = open(crontab,'w')
+            c.write('7-23/10 * * * * python3 '+old+'\n')
+            c.close()
+            os.system('crontab ./crontab')
+            if platform.system() == 'Linux':
+                os.system('service cron restart')
+            else:
+                os.system('sudo /usr/sbin/cron restart')
+                print('If you fail to create a scheduled task, run \'crontab -e\' and enter \'7-23/10 * * * * python3 '+old+'\\auto_nuist.py\', then run \'sudo /usr/sbin/cron restart\' to enabling it!')
+def change_config(username,domain,password,scheduled_task_need):
     shutil.copy2(os.getcwd()+'/auto_inuist.py',os.getcwd()+'/auto_inuist_tmp.py')
     old = os.getcwd()+'/auto_inuist.py'
     new = os.getcwd()+'/auto_inuist_tmp.py'
     o = open(old, mode='r')
     n = open(new, mode='w')
     for line in o:
-        n.write(line.replace('username = input(\'Enter your username(Student number or mobile number):\\n\')', 'username = \''+username+'\'').replace('domain = input(\'Enter your domain(ChinaNet/Unicom/CMCC/NUIST):\\n\')', 'domain = \''+domain+'\'').replace('password = base64.b64encode(str.encode(getpass.getpass(\'Enter your password:\\n\')))', 'password = \''+password.decode()+'\''))
+        n.write(line.replace('username = input(\'Enter your username(Student number or mobile number):\\n\')', 'username = \''+username+'\'')\
+        .replace('domain = input(\'Enter your domain(ChinaNet/Unicom/CMCC/NUIST):\\n\')', 'domain = \''+domain+'\'')\
+        .replace('password = base64.b64encode(str.encode(getpass.getpass(\'Enter your password:\\n\')))', 'password = \''+password.decode()+'\'')\
+        .replace('scheduled_task_need = 1','scheduled_task_need = '+str(scheduled_task_need)))
+        .replace('use_time = 1','use_time = 0'))
     o.close()
     n.close()
     os.remove(old)
     os.rename(new, old)
-def scheduled_task(refuse):
-    shutil.copy2(os.getcwd()+'/auto_inuist.py',os.getcwd()+'/auto_inuist_tmp.py')
-    old = os.getcwd()+'/auto_inuist.py'
-    new = os.getcwd()+'/auto_inuist_tmp.py'
-    o = open(old, mode='r')
-    n = open(new, mode='w')
-    for line in o:
-        n.write(line.replace('scheduled_task_need = 1','scheduled_task_need = 0'))
-    o.close()
-    n.close()
-    os.remove(old)
-    os.rename(new, old)
-    if refuse:
-        if platform.system() == 'Windows':
-            print('Please use the scheduled task to set up automatic login. You need to add auto_nuist.py to the scheduled task.')
-        else:
-            crontab = os.getcwd()+'/crontab'
-            if not os.path.exists(crontab):
-                c = open(crontab,'w')
-                c.write('7-23/10 * * * * python3 '+old+'\n')
-                c.close()
-                os.system('crontab ./crontab')
-                if platform.system() == 'Linux':
-                    os.system('service cron restart')
-                else:
-                    os.system('sudo /usr/sbin/cron restart')
-                    print('If you fail to create a scheduled task, run \'crontab -e\' and enter \'7-23/10 * * * * python3 '+old+'\\auto_nuist.py\', then run \'sudo /usr/sbin/cron restart\' to enabling it!')
 print('Network connection testing...')
 pingtest_local = pingtest('127.0.0.1')
 if pingtest('114.114.114.114') != pingtest_local:
@@ -84,7 +78,10 @@ if pingtest('114.114.114.114') != pingtest_local:
     else:
         print('Authentication server is available, login...')
         scheduled_task_need = 1
+        use_time = 1
         username,domain,password = connect_network()
+        if use_time
+            change_config(username,domain,password,1)
         print('Login is complete! Retest network connections...')
         if pingtest('114.114.114.114') != pingtest_local:
             print('No network connection, please check the account status!')
@@ -92,9 +89,10 @@ if pingtest('114.114.114.114') != pingtest_local:
             print('Network connection completed!')
         if scheduled_task_need:
             if input('Do you need to use scheduled tasks for automatic login now?(Y/N)\n') == "Y":
-                scheduled_task(1)
+                scheduled_task()
+                change_config(username,domain,password,0) 
             else:
                 if input('Do you always refuse to use program certification?(Y/N)\nEnter Y to deny the use of the scheduled task, otherwise you will be asked if you want to use the scheduled task each time you manually run the script until you automatically use the scheduled task.\n') == "Y":
-                    scheduled_task(0)  
+                    change_config(username,domain,password,0) 
 else:
     print('The network status is normal and no authentication is required.')
